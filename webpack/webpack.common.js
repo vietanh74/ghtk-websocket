16e75b12cd8c4de478786c8a9da49f9aee1a5c0b
anhtv56 <anhtv56@ghtk.co>
2022-07-01 17:25:57 +0700

    update

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const dotenv = require('dotenv').config({ path: process.cwd() + '/.env'});
+const TerserPlugin = require("terser-webpack-plugin");
 const webpack = require('webpack');
 
 module.exports = {
@@ -15,6 +16,8 @@ module.exports = {
   optimization: {
     usedExports: true,
     moduleIds: 'deterministic',
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
